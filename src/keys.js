426693a939a7b65187c8b88ef76b4bddcd1427a9
namesphill <acosta_jf@icloud.com>
2020-03-26 14:09:22 -0700

    include .keys

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+module.exports.NOTION_TOKEN = process.env.NOTION_TOKEN;
+
+module.exports.BLOG_INDEX_ID = "5f196dc699af47058a6418e6a435bf0d";
+
+module.exports.USER_ID = "4e919141-98cf-4ec0-b969-3476645cd6e2";
